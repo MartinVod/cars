8e04ba2cf57a4846134178dc7bd84ee5c9bf5327
Martinvod <Martin@branded.co.il>
2021-11-05 17:58:22 +0200

    :patch: clearing logs - no http requests from https website

--- src/utils/api.js
+++ src/utils/api.js
@@ -14,7 +14,6 @@ export async function getLocation() {
     response = await API();
     response.success = true;
   } catch (err) {
-    console.log('err',err)
     response = err.response; // pulls the response from the error
     if (response) response.success = false;
   }
